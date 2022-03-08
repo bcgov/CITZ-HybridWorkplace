d3eafa01bfc13bf7b6602f76ddc7878c60f4bd71
Bettesworth <Jayna.Bettesworth@gov.bc.ca>
2022-03-07 16:19:08 -0800

    added password requirments

--- app/client/src/Components/register.js
+++ app/client/src/Components/register.js
@@ -20,14 +20,12 @@
  * @module
  */
 
-
- import '../App.css';
- import Footer from '../Views/footer';
+  import './Styles/register.css'
  import {useState} from 'react';
  import {useNavigate, Link} from 'react-router-dom';
  
  function App() {
-   const history = useNavigate();
+   const navigate = useNavigate();
    const [name, setName] = useState('');
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
@@ -50,8 +48,68 @@
      const data = await response.json()
      console.log(data);
      if (data.status === 'ok'){
-      window.location='/login'
+      navigate('/login')
      }
+   }
+   
+   async function PasswordRequirment(){
+    const myInput = document.getElementById("psw");
+    const letter = document.getElementById("letter");
+    const capital = document.getElementById("capital");
+    const number = document.getElementById("number");
+    const length = document.getElementById("length");
+    
+    //clicks on the password field, show the message box
+    myInput.onfocus = function() {
+      document.getElementById("message").style.display = "block";
+    }
+    
+    // Clicks outside of the password field, hide the message box
+    myInput.onblur = function() {
+      document.getElementById("message").style.display = "none";
+    }
+    
+    // When the user starts to type something inside the password field
+    myInput.onkeyup = function() {
+      // Validate the lowercase letters
+      const lowerCaseLetters = /[a-z]/g;
+      if(myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+      } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+    
+      // Validate the capital letters
+      const upperCaseLetters = /[A-Z]/g;
+      if(myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+      } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+      }
+    
+      // Validate the numbers
+      const numbers = /[0-9]/g;
+      if(myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+      } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+      }
+    
+      // Validate the length
+      if(myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+      } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+      }
+    }
    }
  
    return (
@@ -61,7 +119,8 @@
        <Link to="/about"> Learn More Here </Link>
        <br/>
        <br/>
-       <h2> Register</h2>
+       <div id='Register'>
+       <h2> Sign Up</h2>
        <form onSubmit={registerUser}>
          <input 
            value={name}
@@ -79,14 +138,27 @@
          <br/>
          <input 
            value={password}
-           onChange={(e) => setPassword(e.target.value)}
+           onChange={(e) => {PasswordRequirment(); setPassword(e.target.value) }}
            type='password'
            placeholder='Password'
+           id="psw" 
+           name="psw" 
+           pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+           title="Password must contain at least one number, uppercase, and lowercase letter, and at least 8 or more characters"
          />
-         <br/>
+
+         
          <br />
-         <input type='submit' value='Submit' />
+         <input type='submit' value='Submit' id='submit' />
        </form>
+       </div>
+       <div id="message">
+          <h4> Password must contain the following:</h4>
+          <p id="letter" className="invalid">One <b>lowercase</b> letter</p>
+          <p id="capital" className="invalid">One <b>uppercase</b> letter</p>
+          <p id="number" className="invalid">One <b>number</b></p>
+          <p id="length" className="invalid">Minimum <b>8 characters</b></p>
+        </div>
        <br />
        <br />
        <p> Already have an account?  
