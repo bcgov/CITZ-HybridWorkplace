ba40e4655d8b1168c94238c75675e76cd0f5fb57
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2022-04-04 16:09:24 -0700

    fix

--- app/client/src/routes.js
+++ app/client/src/routes.js
@@ -28,12 +28,10 @@
  import Home from './views/home';
  import About from './views/aboutPage';
  import ProfilePage from './views/profilePage';
- import EditProfile from './components/editProfile';
- import CreateCommunity from './components/createCommunity';
+ import EditProfile from './Components/editProfile';
+ import CreateCommunity from './Components/createCommunity';
  import CommunitiesPage from './views/communitiesPage';
- import NewCommunity from './components/newCommunity';
- import NewPost from './components/addPost';
- import PostList from './views/Posts'
+ import NewCommunity from './Components/newCommunity';
  
  import React, { Component, } from 'react';
  import { Routes, Route } from 'react-router-dom';
