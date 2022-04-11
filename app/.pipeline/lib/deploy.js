51f47936508bc09023b08e8e014a93560416fe20
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2022-04-11 13:39:38 -0700

    fix

--- app/.pipeline/lib/deploy.js
+++ app/.pipeline/lib/deploy.js
@@ -19,9 +19,7 @@ module.exports = (settings)=>{
   const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
 
   objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/mongodb/dc.yaml`, {
-    'param':{
-      'NAME': 'mongodb',
-    }
+
   }))
 
   objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/server/dc.yaml`, {
