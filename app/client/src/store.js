202e88ab39e4196123965dcbe95c053aa4137745
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2022-04-12 15:45:37 -0700

    fix

--- app/client/src/store.js
+++ app/client/src/store.js
@@ -33,7 +33,7 @@ const store = createStore(
     initialState, 
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //extentions for working with redux on chrome.
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //extentions for working with redux on chrome.
         //comment out above line if you want this to work on other browsers.
         )
 );
