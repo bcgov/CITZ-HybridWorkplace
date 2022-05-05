d8903465179288a85023aacfac94c8676b085aca
Brady <braden.jr.mitch@gmail.com>
2022-05-04 22:01:38 -0700

    Refactored api index.js into index, db, and express files. Fixed api not being able to connect to mongoose

---
+++ app/server/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGO_URI || 'mongodb://mongodb:27017/TheNeighborhood';
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// print message to console when connected to DB 
+mongoose.connection.on('connected', () => { 
+    console.log('Mongoose connected to ' + dbURI); 
+});
+
+// print error message to console
+// if there is a problem connecting 
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongoose connection error:' + err); 
+}); 
+mongoose.connection.on('disconnected', () => { 
+    console.log('Mongoose disconnected'); 
+});
+
+// schema imports go here
