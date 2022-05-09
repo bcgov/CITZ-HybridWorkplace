6a4752a69cd9c2adb48436dc7dd0199d44d6d4f0
Zach Bourque <59429284+ZachBourque@users.noreply.github.com>
2022-05-09 16:03:33 -0700

    Update deploy-api.sh

--- .github/helpers/deploy-api.sh
+++ .github/helpers/deploy-api.sh
@@ -2,7 +2,7 @@
 
 # script to process slam-api deployment config template and deploy it to a given namespace
 
-oc process -f /home/runner/work/citz-imb-slam-api/citz-imb-slam-api/openshift/templates/citz-imb-slam-api/dc.yaml --namespace=$NAMESPACE \
+oc process -f /home/runner/work/citz-hybridworkplace/citz-hybridworkplace/openshift/templates/citz-hybridworkplace/dc.yaml --namespace=$NAMESPACE \
     -p APPLICATION_NAME=$APPLICATION_NAME \
     -p LICENSE_PLATE=$LICENSE_PLATE \
     -p ENVIRONMENT=$ENVIRONMENT | \
