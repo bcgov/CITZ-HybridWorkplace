92c460a08665701838d774a81659a4274e95cdaf
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-10 15:21:52 -0700

    Working tests. Starting to expand tests on App.js

--- app/client/src/tests/App.test.js
+++ app/client/src/tests/App.test.js
@@ -5,38 +5,55 @@ import ReactDOM from 'react-dom';
 import '../index.css';
 import App from '../App';
 import {BrowserRouter} from 'react-router-dom';
+import { isElement } from 'react-dom/test-utils';
 
 
-//Original test case that comes with react-scripts
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
+// //test just to see jest response
+// describe('testing jest responses', () => {
+//   test('intended to fail for jest testing', () => {
+//     expect(3).toEqual(4);
+//   });
+  
+//   test('intended to pass for jest testing', () => {
+//     expect(4).toEqual(4); 
+//   });  
+// });
+
+// test('renders footer with "Accessability" inside', () => {
+//   const linkElement = screen.getByText(/Accessability/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
 
+//Test to ensure footer contains "Accessability" after initial render 
+describe('Testing that elements rendered', () => {
+  beforeAll(() => {
+    render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
 
-//test just to see jest response
-describe('testing jest responses', () => {
-  test('intended to fail for jest testing', () => {
-    expect(3).toEqual(4);
+    );
   });
-  
-  test('intended to pass for jest testing', () => {
-    expect(4).toEqual(4); 
-  });  
+
+  test('footer rendered fully', () => {
+    const footer = document.querySelector('.footer');
+    // const homeLink = screen.getByText(/Home/i);
+    const disclaimerLink = screen.getByText(/Disclaimer/i);
+    const privacyLink = screen.getByText(/Privacy/i);
+    const accessLink = screen.getByText(/Accessability/i);
+    const copyrightLink = screen.getByText(/Copyright/i);
+
+    
+
+    expect(footer.className).toBe('footer');  //this is redundant... if you got footer from above, then of course it does.
+    // expect(homeLink).toBeTruthy();
+    expect(disclaimerLink).toBeInTheDocument();
+    expect(privacyLink).toBeInTheDocument();
+    expect(accessLink).toBeInTheDocument();
+    expect(copyrightLink).toBeInTheDocument();
+  });
 });
 
-//Test to ensure footer contains "Accessability" after initial render
-test('renders footer with "Accessability" inside', () => {
-  render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-  
-  const linkElement = screen.getByText(/Accessability/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
