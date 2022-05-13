0b0d4ef6f1cc640167b1d1eae11baa7c69aa8fa9
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2022-05-13 11:49:47 -0700

    Adds Zach's changes which, for some reason I can't discern, are showing up on my local working branch.

---
+++ app/client/src/components 2/PostsList.js
@@ -0,0 +1,79 @@
+/* 
+ Copyright © 2022 Province of British Columbia
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/**
+ * Application entry point
+ * @author [Jayna Bettesworth](bettesworthjayna@gmail.com)
+ * @module
+ */
+
+ import React, { Component, setState } from 'react';
+ import { connect } from 'react-redux';
+ import { getPosts } from '../actions/postActions';
+ import PropTypes from 'prop-types';
+ import Paper from '@mui/material/Paper';
+ 
+ 
+  class Posts extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          posts: []
+      }
+  }
+
+ 
+     componentDidMount(){
+        //  this.props.getPosts();
+        fetch(`${window._env_.API_REF}/post`)
+             .then(res => res.json())
+             .then(data => (this.setState({posts: data})));
+     }
+   render() {
+       const postItems = this.state.posts.map(post => (
+           <div key={post._id}>
+            <Paper sx={{
+               px: 1,
+               py: 0,
+               margin: 'auto'
+              }}
+              variant="outlined" square >
+               <h3>{post.title}</h3>
+              <p>{post.message}</p>
+              </Paper>   
+              
+           </div>
+       ))
+     return (
+       <div>
+         {postItems}
+       </div>
+     )
+   }
+ }
+ 
+ Posts.propTypes = {
+     getPosts: PropTypes.func.isRequired, 
+     posts: PropTypes.array.isRequired
+ }
+ 
+ const mapStateToProps = state => ({
+     posts: state.posts.items 
+ 
+ });
+ 
+ export default connect(mapStateToProps, {getPosts})(Posts);
+ 
---
+++ app/client/src/components 2/profileInfo.js
@@ -0,0 +1,78 @@
+//
+// Copyright © 2022 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/**
+ * Application entry point
+ * @author [Jayna Bettesworth](bettesworthjayna@gmail.com)
+ * @module
+ */
+ import React, { useEffect, useState } from 'react'
+ import jwt_decode from "jwt-decode";
+ import { useNavigate } from 'react-router-dom';
+
+ const Profile = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [title, setTitle] = useState('')
+    const [name, setName] = useState('')
+    const [fullName, setFullName] = useState('')
+    const [bio, setBio] = useState('')
+
+    async function populateQuote() {
+        const req = await fetch(`${window._env_.API_REF}/profile`, {
+            headers: {
+                'x-access-token': localStorage.getItem('token'),
+            },
+        })
+
+        const data = await req.json()
+        if(data.status === 'ok'){
+            setEmail(data.email)
+            setName(data.name)
+            setTitle(data.title)
+            setFullName(data.fullName)
+            setBio(data.bio)
+        }else{
+            alert(data.error)
+        }
+    }
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token){
+            const user = jwt_decode(token)
+            if(!user){
+                localStorage.removeItem('token')
+                navigate('/login')
+            }else{
+                 
+                populateQuote()
+            }
+        }
+    }, [])
+
+
+     return ( 
+        <div >
+            <h3>{fullName || ''}</h3>
+            <h4>{name}</h4>
+            <p> {email} </p>
+            <p> {title || ''} </p>
+            <br />
+            <p> {bio || ''} </p>
+        </div>
+     )
+ }
+ export default Profile;
