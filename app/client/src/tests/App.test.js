94e3e35f4d1f612da8532886308e1fd9066342ac
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-13 09:42:15 -0700

    Restored original login screen test.

--- app/client/src/tests/App.test.js
+++ app/client/src/tests/App.test.js
@@ -23,24 +23,24 @@ describe('Testing log in, sign up, register front-end behaviours', () => {
   test('clicking log in loads form without sign-up-related fields', () => {
     //identify fields for checking
     let idirInput = screen.queryAllByText(/idir/i);
-    // let passInput = screen.queryAllByText(/Password/i);
-    // let emailInput = screen.queryAllByText(/Email/i);
+    let passInput = screen.queryAllByText(/Password/i);
+    let emailInput = screen.queryAllByText(/Email/i);
     //check if they're there
     expect(idirInput).toHaveLength(1);
-    // expect(passInput).toHaveLength(2);
-    // expect(emailInput).toHaveLength(1);
+    expect(passInput).toHaveLength(2);
+    expect(emailInput).toHaveLength(1);
 
     //click log in
     UserEvent.click(screen.getByText(/Log In/i));
 
     //attempt to reaquire input labels
-    idirInput = screen.queryAllByText(/email/i);
-    let passInput = screen.queryAllByText(/login/i);
-    // emailInput = screen.queryAllByText(/Email/i);
+    idirInput = screen.queryAllByText(/idir/i);
+    passInput = screen.queryAllByText(/password/i);
+    emailInput = screen.queryAllByText(/Email/i);
     //check if they're there
-    expect(idirInput).toHaveLength(0);
+    expect(idirInput).toHaveLength(1);
     expect(passInput).toHaveLength(1);
-    // expect(emailInput).toHaveLength(0); //should be gone
+    expect(emailInput).toHaveLength(0); //should be gone
   });
 
 });
