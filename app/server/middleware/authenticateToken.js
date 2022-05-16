ab568e92afd23a0537c3108606556a3fcd7b10ee
Brady <braden.jr.mitch@gmail.com>
2022-05-16 14:08:06 -0700

    Resolved merge conflicts

---
+++ app/server/middleware/authenticateToken.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/user.model');
+
+const authenticateToken = asyncHandler(async (req, res, next) => {
+    try {
+      // Get token from header
+      let token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+      if (token == null) return res.status(401).send('Not authorized.');
+
+      // Verify token
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).send('Not authorized.');
+        req.user = user
+        next();
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send('Not authorized.');
+    }
+});
+
+module.exports = authenticateToken;
