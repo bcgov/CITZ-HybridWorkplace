57bfbd2e4b3dac4a2089e5936019330a5f611d03
Brady <braden.jr.mitch@gmail.com>
2022-05-16 12:37:54 -0700

    Added mongo init tokens

--- mongo/init/init-db.sh
+++ mongo/init/init-db.sh
@@ -4,4 +4,4 @@ echo "########### Loading data to Mongo DB ###########"
 mongoimport --jsonArray --db TheNeighborhood --collection user-data --file /tmp/data/users.json
 mongoimport --jsonArray --db TheNeighborhood --collection community-data --file /tmp/data/communities.json
 mongoimport --jsonArray --db TheNeighborhood --collection post-data --file /tmp/data/posts.json
-
+mongoimport --jsonArray --db TheNeighborhood --collection refreshTokens --file /tmp/data/tokens.json
