8ddf1ba477434181706a42709077b71797a3ccb5
Brady <braden.jr.mitch@gmail.com>
2022-05-16 13:44:44 -0700

    Testing database

--- app/server/models/refreshTokens.model.js
+++ app/server/models/refreshTokens.model.js
@@ -25,6 +25,7 @@ const mongoose = require('mongoose');
 const Tokens = new mongoose.Schema(
 	{
 		token: { type: String, required: true },
+		user: { type: String, required: true },
 	},
 	{ collection: 'refreshTokens' }
 );
