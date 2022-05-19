b8e2cce77b33aa6db5c7198ed432b781120f000d
Brady <braden.jr.mitch@gmail.com>
2022-05-18 22:55:11 -0700

    Added healthcheck for mongodb container in dockercompose

--- mongo/init/init-db.sh
+++ mongo/init/init-db.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "########### Loading data to Mongo DB ###########"
-#mongoimport --jsonArray --db TheNeighbourhood --collection user --file /tmp/data/users.json
-mongoimport --jsonArray --db TheNeighbourhood --collection community --file /tmp/data/communities.json
-#mongoimport --jsonArray --db TheNeighbourhood --collection post --file /tmp/data/posts.json
+mongoimport --jsonArray --db $MONGO_DATABASE_NAME --collection community --file /tmp/data/communities.json
