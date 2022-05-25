3e4d77cf0751e28880d7bf743f1f27082b014d77
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-25 15:13:36 -0700

    Wrote test to see if token was received.

--- app/server/tests/api-login.test.js
+++ app/server/tests/api-login.test.js
@@ -13,4 +13,19 @@ describe('Testing optimal inputs for login', () => {
         });
         expect(response.status).toBe(201);
     });
+
+    test('Token and refresh token are received upon login', async () => {
+        let response = await request.post('/')
+        .send({
+            "name": "test",
+            "password": "Test123!"
+        });
+        let token = response.body.token;
+        let refreshToken = response.body.refreshToken;
+
+        expect(token).toBeTruthy();
+        expect(typeof token).toBe('string');
+        expect(refreshToken).toBeTruthy();
+        expect(typeof refreshToken).toBe('string');
+    });
 });
