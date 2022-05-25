616551e17034b1cf64b1cc07c936ab9365cda9cf
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-25 14:58:46 -0700

    Formatting change.

--- app/server/tests/api-register.test.js
+++ app/server/tests/api-register.test.js
@@ -10,11 +10,21 @@ const request = supertest(endpoint);
 describe('Testing optimal inputs for register', () => {
     //name is for the IDIR input
 
-    test('new users are added successfully', () => {
+    test('new users are registered successfully - returns 201', async () => {
+        //generate random name and email
+        let name = (Math.random() + 1).toString(36).substring(15);
+        let email = (Math.random() + 1).toString(36).substring(15);
 
+        let response = await request.post('/')
+            .send({
+                "name": `"${name}"`,
+                "email": `"${email}@gov.bc.ca"`,
+                "password": "MyDogHasFleas1!"
+            });
+        expect(response.status).toBe(201);
     });
 
-    test('API returns 403 when user already exists', async () => {
+    test('user attempts to register with name and password already used - returns 403', async () => {
         let response = await request.post('/')
             .send({
                 "name": "Zach Galafianakis",
