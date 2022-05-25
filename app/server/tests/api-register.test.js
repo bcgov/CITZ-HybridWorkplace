7ae0327f62c83a91106ca09b691ce5de0aedc4b9
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-25 13:12:18 -0700

    Adding additional test cases.

--- app/server/tests/api-register.test.js
+++ app/server/tests/api-register.test.js
@@ -7,8 +7,13 @@ const endpoint = 'https://hwp-express-api-d63404-dev.apps.silver.devops.gov.bc.c
 const supertest = require('supertest');
 const request = supertest(endpoint);
 
-describe('Testing expected outputs for register', () => {
+describe('Testing optimal inputs for register', () => {
     //name is for the IDIR input
+
+    test('new users are added successfully', () => {
+
+    });
+
     test('API returns 403 when user already exists', async () => {
         let response = await request.post('/')
             .send({
@@ -17,5 +22,15 @@ describe('Testing expected outputs for register', () => {
                 "password": "MyDogHasFleas1!"
             });
         expect(response.status).toBe(403); //user already exists
+    });
+});
+
+describe('Testing sub-optimal inputs for register', () => {
+    test('api rejects registration if password doesn\'t meet specifications', () => {
+
+    });
+
+    test('api rejects registration if email is not valid', () => {
+
     });
 });
