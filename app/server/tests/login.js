2451fa352f4781d5b16dc50c39bec90391974302
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-25 15:28:13 -0700

    Added login file for later testing ease.

---
+++ app/server/tests/login.js
@@ -0,0 +1,14 @@
+const endpoint = 'https://hwp-express-api-d63404-dev.apps.silver.devops.gov.bc.ca/api/login';
+const supertest = require('supertest');
+const request = supertest(endpoint);
+
+function login(){
+    let response = await request.post('/')
+        .send({
+            "name": "test",
+            "password": "Test123!"
+        });
+    return response;
+}
+
+module.exports = { login };
