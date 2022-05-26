2a9614068be39e90541c1cc7d795caf7a3f32744
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-26 10:36:28 -0700

    Working auth and cookie for token request.

--- app/server/tests/api-token.test.js
+++ app/server/tests/api-token.test.js
@@ -1,17 +1,30 @@
-import {login} from './login';
+// import {login} from './login';
 
-const endpoint = 'https://hwp-express-api-d63404-dev.apps.silver.devops.gov.bc.ca/api/token';
+const endpoint = 'https://hwp-express-api-d63404-dev.apps.silver.devops.gov.bc.ca/api';
 const supertest = require('supertest');
 const request = supertest(endpoint);
 
 //import login function
 // const login = require('./login');
-const loginResponse = login();
+// const loginResponse = login();
 
 describe('Testing sending of refresh token', () => {
     test('Sending refresh token returns new auth token', async () => {
-        console.log(loginResponse);
-        let response = await request.get('/')
-            .set('Authorization', `'bearer ${loginResponse.body.token}'`);
+        let loginResponse = await request.post('/login')
+        .send({
+            "name": "test",
+            "password": "Test123!"
+        });
+
+        let response = await request.get('/token')
+            .set('Authorization', `bearer ${loginResponse.body.token}`)
+            .set('Cookie', `jwt=${loginResponse.body.refreshToken}`);
+        console.log(loginResponse.headers);
+        console.log(loginResponse.body);
+        
+        console.log(response.headers);
+        console.log(response.body);
+        expect(typeof response).toBe('object');
+        expect(response.status).toBe(200);
     });
 });
