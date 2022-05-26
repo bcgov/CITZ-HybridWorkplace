9368fdbfd1934590c41490b4168dd896f0cd5c31
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-26 08:48:56 -0700

    Testing with imported login, trying token

--- app/server/tests/api-token.test.js
+++ app/server/tests/api-token.test.js
@@ -1,13 +1,17 @@
+import {login} from './login';
+
 const endpoint = 'https://hwp-express-api-d63404-dev.apps.silver.devops.gov.bc.ca/api/token';
 const supertest = require('supertest');
 const request = supertest(endpoint);
 
 //import login function
-const loginResponse = require('./login');
+// const login = require('./login');
+const loginResponse = login();
 
 describe('Testing sending of refresh token', () => {
     test('Sending refresh token returns new auth token', async () => {
+        console.log(loginResponse);
         let response = await request.get('/')
-            .set('Authorization', 'bearer ' + loginResponse.body.refreshToken);
+            .set('Authorization', `'bearer ${loginResponse.body.token}'`);
     });
 });
