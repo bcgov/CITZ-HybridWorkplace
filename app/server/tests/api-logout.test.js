72f57a241f1f7938f90aeae02d5a0ade0ab3b81e
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-26 11:48:55 -0700

    Two test conditions: marked as TODO

--- app/server/tests/api-logout.test.js
+++ app/server/tests/api-logout.test.js
@@ -28,4 +28,21 @@ describe('Testing logout endpoint', () => {
         expect(response.status).toBe(401);
     });
 
+    //TODO: Update this? Currently fails because that same cookie is live after logout. Could be used to refresh token.
+    xtest('Logging out with a previously used cookie - should not return 204', async () => {
+        let response = await request.get('/logout')
+            .set('accept', `*/*`)
+            .set('Cookie', `jwt=${loginResponse.body.refreshToken}`);
+
+        expect(response.status).not.toBe(204);
+    });
+
+    //TODO: Currently returns 502. 
+    xtest('Logging out with an invalid cookie - returns 401', async () => {
+        let response = await request.get('/logout')
+            .set('accept', `*/*`)
+            .set('Cookie', `jwt=dsjfklsi3hkj3l24hkl32hjk324hjk324hjk324hu32njk32`);
+
+        expect(response.status).toBe(401);
+    });
 });
