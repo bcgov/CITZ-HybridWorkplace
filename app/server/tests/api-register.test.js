f33204897d11916b886e63ba043d7c8accf255c4
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-26 14:38:52 -0700

    Initial tests with cleaning up created users.

--- app/server/tests/api-register.test.js
+++ app/server/tests/api-register.test.js
@@ -10,18 +10,41 @@ const request = supertest(endpoint);
 describe('Testing optimal inputs for register', () => {
     //name is for the IDIR input
 
+    // Try to insert fake user if it's not already done.
+    beforeAll(async () => {
+        await request.post('/register')
+            .send({
+                "name": "Zach Galafianakis",
+                "email": "zgalafianakis@gov.bc.ca",
+                "password": "MyDogHasFleas1!"
+            });
+    });
+
     test('new users are registered successfully - returns 201', async () => {
         //generate random name and email
-        let name = (Math.random() + 1).toString(36).substring(15);
-        let email = (Math.random() + 1).toString(36).substring(15);
+        let name = (Math.random() + 1).toString(36).substring(2);
 
         let response = await request.post('/register')
             .send({
-                "name": `"${name}"`,
-                "email": `"${email}@gov.bc.ca"`,
+                "name": `${name}`,
+                "email": `${name}@gov.bc.ca`,
                 "password": "MyDogHasFleas1!"
             });
         expect(response.status).toBe(201);
+
+        let loginResponse = await request.post('/login')
+        .send({
+            "name": `${name}`,
+            "password": "MyDogHasFleas1!"
+        });
+
+        expect(loginResponse.status).toBe(201);
+
+        let deleteResponse = await request.delete(`/user/${name}`)
+                        .set('accept', '*/*')
+                        .set('Authorization', `bearer ${loginResponse.body.token}`);
+
+        expect(deleteResponse.status).toBe(204);
     });
 
     test('user attempts to register with name and password already used - returns 403', async () => {
