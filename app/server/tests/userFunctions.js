b438816528f06214db1639541bae24dd7168b932
dbarkowsky <dylanbarkowsky@gmail.com>
2022-05-27 11:29:25 -0700

    Adjusted some areas for style. Added comments.

--- app/server/tests/userFunctions.js
+++ app/server/tests/userFunctions.js
@@ -3,12 +3,13 @@ const supertest = require('supertest');
 const request = supertest(endpoint);
 
 class UserFunctions{
-    registerList;
+    registerList; // Tracks registered users for later clean up.
 
     constructor(){
         this.registerList = [];
     }
 
+    // Registers a user. Adds this new user to registerList for later clean up.
     async register(name, email, password) {
         let registerResponse = await request.post('/register')
                                     .send({
@@ -27,12 +28,14 @@ class UserFunctions{
         return registerResponse;
     }
 
+    // Deletes any users registered in this instance of UserFunctions
     async deleteUsers() {
         for (let i = 0; i < this.registerList.length; i++){
             await this.delete(this.registerList[i].user.name, this.registerList[i].user.password);
         }
     }
 
+    // Logs an existing user in
     async login(name, password){
         let loginResponse = await request.post('/login')
             .set('accept', 'application/json')
@@ -44,28 +47,31 @@ class UserFunctions{
         return loginResponse;
     }
 
+    // Deletes an existing user
     async delete(name, password){
         let loginResponse = await this.login(name, password);
 
-        let deleteResponse = await request.delete(`/user/${name}`)
+        let deleteResponse = await request.delete(`/user/${ name }`)
                             .set('accept', '*/*')
-                            .set('Authorization', `bearer ${loginResponse.body.token}`);
+                            .set('Authorization', `bearer ${ loginResponse.body.token }`);
         return deleteResponse;
     }
 
+    // Logs out an existing user
     async logout(name, password){
         let loginResponse = await this.login(name, password);
         let logoutResponse = await request.get('/logout')
             .set('accept', `*/*`)
-            .set('Cookie', `jwt=${loginResponse.body.refreshToken}`);
+            .set('Cookie', `jwt=${ loginResponse.body.refreshToken }`);
         return logoutResponse;
     }
 
+    // Retrieves a new token for an existing user
     async token(name, password){
         let loginResponse = await this.login(name, password);
         let tokenResponse = await request.get('/token')
-            .set('Authorization', `bearer ${loginResponse.body.token}`)
-            .set('Cookie', `jwt=${loginResponse.body.refreshToken}`);
+            .set('Authorization', `bearer ${ loginResponse.body.token }`)
+            .set('Cookie', `jwt=${ loginResponse.body.refreshToken }`);
         return tokenResponse;
     }
 }
