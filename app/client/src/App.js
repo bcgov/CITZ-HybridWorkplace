e933e15b8a24ab2f9d7ad59de063950d08fa9d1f
ZachBourque <zachbourque01@gmail.com>
2022-05-30 14:40:26 -0700

    Removed merge conflicts

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -85,6 +85,18 @@ useEffect(() => {
             </div>
             <Footer />
           </div>
+          {store.getState().alerts?.errors?.map((item) => (
+            <div>
+              <h1>{item}</h1>
+              {console.log(item)}
+            </div>
+          ))}
+          {store.getState().alerts?.successes?.map((item) => (
+            <div>
+              <h1>{item}</h1>
+              {console.log(item)}
+            </div>
+          ))}
         </Provider>
       </Paper>
     </ThemeProvider>
