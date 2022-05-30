923212895f57664b1d3b179832b73f68b82d4f50
ZachBourque <zachbourque01@gmail.com>
2022-05-30 09:24:59 -0700

    Created alertText.js

---
+++ app/client/src/alertText.js
@@ -0,0 +1,6 @@
+export const INVALID_LOGIN_ERROR = "Invalid IDIR/Password, please try again.";
+// export const _ERROR = ""
+// export const _ERROR = ""
+// export const _ERROR = ""
+// export const _ERROR = ""
+// export const _ERROR = ""
