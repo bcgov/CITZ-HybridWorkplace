aa356d0e32724407a14d523b4943faa69c86e70e
ZachBourque <zachbourque01@gmail.com>
2022-05-30 14:57:48 -0700

    Deleted file client/src/alertText.js

--- app/client/src/alertText.js
+++
@@ -1,6 +0,0 @@
-export const INVALID_LOGIN_ERROR = "Invalid IDIR/Password, please try again.";
-// export const _ERROR = ""
-// export const _ERROR = ""
-// export const _ERROR = ""
-// export const _ERROR = ""
-// export const _ERROR = ""
