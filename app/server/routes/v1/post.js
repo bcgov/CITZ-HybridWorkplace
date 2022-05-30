13009798e385bfb47d027df059af1c3119187fc0
Brady <braden.jr.mitch@gmail.com>
2022-05-29 20:24:48 -0700

    User can untag post

--- app/server/routes/v1/post.js
+++ app/server/routes/v1/post.js
@@ -535,32 +535,25 @@ router.post("/tags/:id", async (req, res) => {
  *        - bearerAuth: []
  *      tags:
  *        - Post
- *      summary: Remove post tag by post id
+ *      summary: Untag post by id
  *      parameters:
  *        - in: path
  *          required: true
  *          name: id
  *          schema:
  *            $ref: "#/components/schemas/Post/properties/id"
- *        - in: query
- *          required: true
- *          name: tag
- *          schema:
- *            $ref: '#/components/schemas/Post/properties/tags/items/properties/tag'
  *      responses:
  *        '404':
- *          description: User not found. OR Post not found. OR Tag not found in query.
- *        '401':
- *          description: Not Authorized. Only creator of post can edit post.
+ *          description: User not found. OR Post not found.
  *        '403':
- *          description: Forbidden. A community can't have more than 7 tags. OR Forbidden. No duplicate tags.
+ *          description: User has not tagged post
  *        '204':
- *          description: Successfully removed tag.
+ *          description: Successfully untagged post.
  *        '400':
  *          description: Bad Request.
  */
 
-// Remove post tag by post id
+// Untag post by id
 router.delete("/tags/:id", async (req, res) => {
   try {
     // TODO: MODERATORS CAN EDIT POST TOO
@@ -569,16 +562,48 @@ router.delete("/tags/:id", async (req, res) => {
 
     if (!user) return res.status(404).send("User not found.");
     if (!post) return res.status(404).send("Post not found.");
-    if (!req.query.tag) return res.status(404).send("Tag not found in query.");
 
-    if (post.creator !== user.id)
-      return res
-        .status(401)
-        .send("Not Authorized. Only creator of post can edit post.");
+    // Check user has tagged post
+    if (
+      !(await Post.exists({
+        _id: post.id,
+        "tags.taggedBy": user.id,
+      }))
+    )
+      return res.status(403).send("User has not tagged post.");
 
-    await Post.updateOne(
+    const selectedTag = await Post.findOne(
       { _id: post.id },
-      { $pull: { tags: { tag: req.query.tag } } }
+      { tags: { $elemMatch: { taggedBy: user.id } } }
+    );
+
+    // Remove user from taggedBy
+    await Post.updateOne(
+      { _id: post.id, tags: { $elemMatch: { taggedBy: user.id } } },
+      { $pull: { "tags.$.taggedBy": user.id } }
+    );
+
+    // Remove tag from post if taggedBy is length 1 (only user)
+    if (selectedTag.tags[0].taggedBy.length === 1) {
+      // Remove tag
+      await Post.updateOne(
+        {
+          _id: post.id,
+          // eslint-disable-next-line no-underscore-dangle
+          tags: { $elemMatch: { _id: selectedTag.tags[0]._id } },
+        },
+        // eslint-disable-next-line no-underscore-dangle
+        { $pull: { tags: { _id: selectedTag.tags[0]._id } } }
+      );
+    }
+
+    // Decrement tag count in community
+    await Community.updateOne(
+      {
+        title: post.community,
+        tags: { $elemMatch: { tag: selectedTag.tags[0].tag } },
+      },
+      { $inc: { "tags.$.count": -1 } }
     );
 
     return res.sendStatus(204);
