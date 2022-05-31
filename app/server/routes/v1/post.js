1e8b3334be8ccfbc8012af4cc1c25e9096bceff6
Brady <braden.jr.mitch@gmail.com>
2022-05-30 23:20:54 -0700

    Added flagging to posts

--- app/server/routes/v1/post.js
+++ app/server/routes/v1/post.js
@@ -632,4 +632,209 @@ router.delete("/tags/:id", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * paths:
+ *  /api/post/flags/{id}:
+ *    get:
+ *      security:
+ *        - bearerAuth: []
+ *      tags:
+ *        - Post
+ *      summary: Get post flags by post id.
+ *      parameters:
+ *        - in: path
+ *          required: true
+ *          name: id
+ *          schema:
+ *            $ref: "#/components/schemas/Post/properties/id"
+ *      responses:
+ *        '404':
+ *          description: Post not found.
+ *        '200':
+ *          description: Post successfully found.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Post/properties/flags'
+ *        '400':
+ *          description: Bad Request.
+ */
+
+// Get post flags by post id
+router.get("/flags/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({
+      _id: req.params.id,
+    }).exec();
+
+    if (!post) return res.status(404).send("Post not found.");
+
+    return res.status(200).json(post.flags);
+  } catch (err) {
+    return res.status(400).send(`Bad Request: ${err}`);
+  }
+});
+
+/**
+ * @swagger
+ * paths:
+ *  /api/post/flags/{id}:
+ *    post:
+ *      security:
+ *        - bearerAuth: []
+ *      tags:
+ *        - Post
+ *      summary: Flag post by post id.
+ *      parameters:
+ *        - in: path
+ *          required: true
+ *          name: id
+ *          schema:
+ *            $ref: '#/components/schemas/Post/properties/id'
+ *        - in: query
+ *          required: true
+ *          name: flag
+ *          schema:
+ *            $ref: '#/components/schemas/Post/properties/flags/items/properties/flag'
+ *      responses:
+ *        '404':
+ *          description: User not found. **||** <br>Post not found.
+ *        '403':
+ *          description: Invalid flag. Use one of <br>[Inappropriate, Hate, Harassment or Bullying, Spam, Misinformation, Against Community Rules]
+ *        '204':
+ *          description: Successfully set flag.
+ *        '400':
+ *          description: Bad Request.
+ */
+
+// Flag post by post id
+router.post("/flags/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.user.name });
+    const post = await Post.findOne({
+      _id: req.params.id,
+    }).exec();
+
+    if (!user) return res.status(404).send("User not found.");
+    if (!post) return res.status(404).send("Post not found.");
+
+    // TODO: Set flags in an options collection, that can be edited by admins
+    const flags = [
+      "Inappropriate",
+      "Hate",
+      "Harassment or Bullying",
+      "Spam",
+      "Misinformation",
+      "Against Community Rules",
+    ];
+
+    if (!flags.includes(req.query.flag))
+      return res
+        .status(403)
+        .send(
+          "Invalid flag. Use one of [Inappropriate, Hate, Harassment or Bullying, Spam, Misinformation, Against Community Rules]"
+        );
+
+    // If flag isn't set on post
+    if (
+      !(await Post.exists({
+        _id: post.id,
+        "flags.flag": req.query.flag,
+      }))
+    ) {
+      // Create flag
+      await Post.updateOne(
+        { _id: post.id },
+        { $push: { flags: { flag: req.query.flag, flaggedBy: [user.id] } } }
+      );
+    } else {
+      // Add user to flaggedBy
+      await Post.updateOne(
+        {
+          _id: post.id,
+          flags: { $elemMatch: { flag: req.query.flag } },
+        },
+        { $addToSet: { "flags.$.flaggedBy": [user.id] } }
+      );
+    }
+
+    return res.sendStatus(204);
+  } catch (err) {
+    return res.status(400).send(`Bad Request: ${err}`);
+  }
+});
+
+/**
+ * @swagger
+ * paths:
+ *  /api/post/flags/{id}:
+ *    delete:
+ *      security:
+ *        - bearerAuth: []
+ *      tags:
+ *        - Post
+ *      summary: Unset flag on post by post id.
+ *      parameters:
+ *        - in: path
+ *          required: true
+ *          name: id
+ *          schema:
+ *            $ref: '#/components/schemas/Post/properties/id'
+ *        - in: query
+ *          required: true
+ *          name: flag
+ *          schema:
+ *            $ref: '#/components/schemas/Post/properties/flags/items/properties/flag'
+ *      responses:
+ *        '404':
+ *          description: User not found. **||** <br>Post not found. **||** <br>Flag not found in query.
+ *        '403':
+ *          description: User has not flagged post with specified flag.
+ *        '204':
+ *          description: Successfully unset flag.
+ *        '400':
+ *          description: Bad Request.
+ */
+
+// Unset flag on post by post id
+router.delete("/flags/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.user.name });
+    const post = await Post.findOne({
+      _id: req.params.id,
+    }).exec();
+
+    if (!user) return res.status(404).send("User not found.");
+    if (!post) return res.status(404).send("Post not found.");
+    if (!req.query.flag)
+      return res.status(404).send("Flag not found in query.");
+
+    // Check user has flagged post
+    if (
+      !(await Post.exists({
+        _id: post.id,
+        "flags.flag": req.query.flag,
+        "flags.flaggedBy": user.id,
+      }))
+    )
+      return res
+        .status(403)
+        .send("User has not flagged post with specified flag.");
+
+    // Remove user from flaggedBy
+    await Post.updateOne(
+      {
+        _id: post.id,
+        flags: { $elemMatch: { flag: req.query.flag } },
+      },
+      { $pull: { "flags.$.flaggedBy": user.id } }
+    );
+
+    return res.sendStatus(204);
+  } catch (err) {
+    return res.status(400).send(`Bad Request: ${err}`);
+  }
+});
+
 module.exports = router;
