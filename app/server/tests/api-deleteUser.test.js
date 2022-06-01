b946a9d4aa28c6d22404bc2dddf34cd11525fbd3
dbarkowsky <dylanbarkowsky@gmail.com>
2022-06-01 13:23:26 -0700

    Added delete, get, patch user tests.

---
+++ app/server/tests/api-deleteUser.test.js
@@ -0,0 +1,48 @@
+const { AuthFunctions } = require('./functions/authFunctions');
+const user = require('./functions/userFunctions');
+const { password, name, email } = require('./functions/randomizer');
+
+let auth = new AuthFunctions();
+
+jest.setTimeout(10000); // Some register-login-action groups take longer than 5000ms default.
+
+describe('Delete users with /user/{name}', () => {
+    let userName = name.gen();
+    let userPassword = password.gen();
+    let userEmail = email.gen();
+    let loginResponse;
+    let response; 
+    beforeAll(async () => {
+        await auth.register(userName, userEmail, userPassword);
+        loginResponse = await auth.login(userName, userPassword);
+    });
+
+    afterAll(async () => {
+        await auth.deleteUsers();
+    });
+
+    test('Trying to delete a user other than yourself should be rejected - returns 401', async () => {
+        await auth.register('Todd', 'todd@gmail.com', 'Todd123!'); // Create new user
+        response = await user.deleteUserByName(loginResponse.body.token, 'Todd'); // Try to delete user with original token
+        expect(response.statusCode).toBe(401); // Not authorized
+    });
+
+    test('User is deleted upon request - returns 204', async () => {
+        response = await user.deleteUserByName(loginResponse.body.token, userName);
+        expect(response.statusCode).toBe(204);
+    });
+
+    test('Trying to delete a user that no longer exists should be rejected - returns 404', async () => {
+        await auth.register('Josie', 'josie@gmail.com', 'josie123!'); // Create new user
+        let tempLoginResponse = await auth.login('Josie', 'josie123!'); // Log them in
+        await user.deleteUserByName(tempLoginResponse.body.token, 'Josie'); // Delete them
+        response = await user.deleteUserByName(tempLoginResponse.body.token, 'Josie'); // Delete them again
+        expect(response.statusCode).toBe(404); // User not found
+    });
+
+    test('Trying to delete a user with an invalid token should be rejected - returns 403', async () => {
+        await auth.register('Josie', 'josie@gmail.com', 'josie123!'); // Create new user
+        response = await user.deleteUserByName('reallybadtokenimeansobadithurts', 'Josie'); // Try to delete user with bad token
+        expect(response.statusCode).toBe(403); // Forbidden
+    });
+}); 
---
+++ app/server/tests/functions/userFunctions.js
@@ -0,0 +1,48 @@
+const endpoint = process.env.API_REF;
+const supertest = require('supertest');
+const request = supertest(endpoint);
+
+function getUser(token){
+    return request.get('/user')
+        .set('accept', 'application/json')
+        .set('Authorization', `bearer ${ token }`);
+}
+
+function editUserByFields(token, email, first_name, last_name, bio, title, quote){
+    let body = {};
+
+    if (email) body.email = email;
+    if (first_name) body.first_name = first_name;
+    if (last_name) body.last_name = last_name;
+    if (bio) body.bio = bio;
+    if (title) body.title = title;
+    if (quote) body.quote = quote;
+
+    return request.patch('/user')
+        .set('accept', 'application/json')
+        .set('content-type', 'application/json')
+        .set('Authorization', `bearer ${ token }`)
+        .send(body);
+}
+
+function editUserByObject(token, body){
+    return request.patch('/user')
+        .set('accept', '*/*')
+        .set('content-type', 'application/json')
+        .set('Authorization', `bearer ${ token }`)
+        .send(body);
+}
+
+function getUserByName(token, name){
+    return request.get(`/user/${ name }`)
+        .set('accept', 'application/json')
+        .set('Authorization', `bearer ${ token }`);
+}
+
+function deleteUserByName(token, name){
+    return request.delete(`/user/${ name }`)
+        .set('accept', '*/*')
+        .set('Authorization', `bearer ${ token }`);
+}
+
+module.exports = { getUser, editUserByFields, editUserByObject, getUserByName, deleteUserByName };
