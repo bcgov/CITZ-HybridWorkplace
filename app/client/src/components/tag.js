48d7372f7b42eb44a348d9e0b6bf43002c3855a5
ZachBourque <zachbourque01@gmail.com>
2022-06-04 13:02:06 -0700

    Setup groundwork for post tagging

--- app/client/src/components/tag.js
+++ app/client/src/components/tag.js
@@ -1,12 +1,26 @@
-import React from "react";
+import { Chip } from "@mui/material";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { addTagToPost } from "../redux/ducks/postDuck";
 
 export const tag = (props) => {
-  return <div>tag</div>;
+  const [clicked, setClicked] = useState(false);
+  const handleTagClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <Chip
+      label={props.name}
+      color={props.color || "primary"}
+      variant={clicked ? "filled" : "outlined"}
+      onClick={handleTagClick}
+    ></Chip>
+  );
 };
 
 const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { addTagToPost };
 
 export default connect(mapStateToProps, mapDispatchToProps)(tag);
---
+++ app/client/src/redux/ducks/tagDuck.js
@@ -0,0 +1,223 @@
+/* 
+ Copyright © 2022 Province of British Columbia
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/**
+ * Application entry point
+ * @author [Zach Bourque](zachbourque01@gmail.com)
+ * @module
+ */
+
+const SET_COMMUNITIES = "CITZ-HYBRIDWORKPLACE/COMMUNITY/SET_COMMUNITIES";
+const SET_USERS_COMMUNITIES =
+  "CITZ-HYBRIDWORKPLACE/COMMUNITY/SET_USERS_COMMUNITIES";
+const ADD_COMMUNITY = "CITZ-HYBRIDWORKPLACE/COMMUNITY/ADD_COMMUNITY";
+const JOIN_COMMUNITY = "CITZ-HYBRIDWORKPLACE/COMMUNITY/JOIN_COMMUNITY";
+const LEAVE_COMMUNITY = "CITZ-HYBRIDWORKPLACE/COMMUNITY/LEAVE_COMMUNITY";
+
+const noTokenText = "Trying to access accessToken, no accessToken in store";
+
+const apiURI = !window._env_.REACT_APP_LOCAL_DEV
+  ? `${window._env_.REACT_APP_API_REF}`
+  : `http://${window._env_.REACT_APP_API_REF}:${window._env_.REACT_APP_API_PORT}`;
+
+export const getCommunities = () => async (dispatch, getState) => {
+  let successful = true;
+  try {
+    const token = getState().auth.accessToken;
+    if (!token) throw new Error(noTokenText);
+
+    const response = await fetch(`${apiURI}/api/community`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    const communities = await response.json();
+
+    dispatch({
+      type: SET_COMMUNITIES,
+      payload: communities,
+    });
+  } catch (err) {
+    console.error(err);
+    successful = false;
+  } finally {
+    return successful;
+  }
+};
+
+export const getUsersCommunities = () => async (dispatch, getState) => {
+  let successful = true;
+  try {
+    const token = getState().auth.accessToken;
+    if (!token) throw new Error(noTokenText);
+
+    const response = await fetch(`${apiURI}/api/community?user=true`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    const communities = await response.json();
+
+    dispatch({
+      type: SET_USERS_COMMUNITIES,
+      payload: communities,
+    });
+  } catch (err) {
+    console.error(err);
+    successful = false;
+  } finally {
+    return successful;
+  }
+};
+
+export const createCommunity =
+  (communityData) => async (dispatch, getState) => {
+    let successful = true;
+    try {
+      const authState = getState().auth;
+      const token = authState.accessToken;
+      const username = authState.user.username;
+      if (!token) throw new Error(noTokenText);
+
+      const response = await fetch(`${apiURI}/api/community`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          title: communityData.title,
+          description: communityData.description,
+          rules: communityData.rules,
+        }),
+      });
+
+      if (!response.ok)
+        throw new Error(`${response.status} ${response.statusText}`);
+
+      const community = await response.json();
+
+      dispatch({
+        type: ADD_COMMUNITY,
+        payload: community,
+      });
+
+      dispatch({
+        type: JOIN_COMMUNITY,
+        payload: community.title,
+      });
+    } catch (err) {
+      console.error(err);
+      successful = false;
+    } finally {
+      return successful;
+    }
+  };
+
+export const joinCommunity = (communityName) => async (dispatch, getState) => {
+  let successful = true;
+  try {
+    const appState = getState();
+    const token = appState.auth.accessToken;
+
+    if (!token) throw new Error(noTokenText);
+
+    const response = await fetch(
+      `${apiURI}/api/community/members/join/${communityName}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok)
+      throw new Error(`${response.status} ${response.statusText}`);
+
+    dispatch({
+      type: JOIN_COMMUNITY,
+      payload: communityName,
+    });
+  } catch (err) {
+    console.error(err);
+    successful = false;
+  } finally {
+    return successful;
+  }
+};
+
+export const leaveCommunity = (communityName) => async (dispatch, getState) => {
+  let successful = true;
+  try {
+    const authState = getState().auth;
+    const token = authState.accessToken;
+    if (!token) throw new Error(noTokenText);
+
+    const response = await fetch(
+      `${apiURI}/api/community/members/leave/${communityName}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok)
+      throw new Error(`${response.status} ${response.statusText}`);
+
+    dispatch({
+      type: LEAVE_COMMUNITY,
+      payload: communityName,
+    });
+  } catch (err) {
+    console.error(err);
+    successful = false;
+  } finally {
+    return successful;
+  }
+};
+
+const initialState = {
+  usersCommunities: [], //users communities
+  items: [], //communitys
+  item: {}, //single community
+};
+
+export function tagReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_COMMUNITIES:
+      return {
+        ...state,
+        items: action.payload,
+      };
+    default:
+      return state;
+  }
+}
