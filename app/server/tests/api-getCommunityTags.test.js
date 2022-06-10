a42176d0dab1abb066aa2a8f47f1f599c1d36ae4
Matthew Sielecki <msielecki2@gmail.com>
2022-06-10 10:55:58 -0700

    created tag tests

---
+++ app/server/tests/api-getCommunityTags.test.js
@@ -0,0 +1,73 @@
+let community = require('./functions/communityFunctions.js');
+let { AuthFunctions } = require('./functions/authFunctions.js');
+let user = new AuthFunctions();
+let token = '';
+
+const welComTitle = "Welcome";
+const welComTags = "Informative";
+
+
+const newComTitle = "hello get Tags";
+const newComDescript = "world";
+const newComRules = "1. rules";
+const newComTags = [];
+
+
+
+describe('Logging in the test user', () => {
+  test('API returns a successful response - code 201', async () => {
+    let response = await user.login('test2', 'Tester123!');
+    token = response.body.token;
+    expect(response.status).toBe(201);
+  });
+});
+
+
+// Testing the get communities function after logging in
+describe('Get Communities Tags  - After Login', () => {
+  let response = '';
+
+  beforeAll( async() => {
+    response = await community.getCommunityTags(welComTitle, token);
+  });
+
+  test('API returns a successful response - code 200', () => {
+    expect(response.status).toBe(200);
+  });
+
+  test('API returns the "Welcome" tags in its response body', () => {
+    expect(" " + response.text+ " ").toContain("[]");
+  });
+});
+
+describe('Creating new Community', () => {
+    test('API returns a successful response - code 201', async() => {
+        await community.deleteCommunity(newComTitle, token);
+        response = await community.createCommunity(newComTitle, newComDescript, newComRules, newComTags, token);
+        expect(response.status).toBe(201);
+    });
+});
+
+// Testing the get communities function after logging in
+describe('Get Community Tags - After Login on new community', () => {
+    let response = '';
+  
+    beforeAll( async() => {
+      response = await community.getCommunityTags(newComTitle, token);
+    });
+  
+    test('API returns a successful response - code 200', () => {
+      expect(response.status).toBe(200);
+    });
+  
+    test('API returns the new community tags its response body', () => {
+      expect(" " + response.text+ " ").toContain("" + newComTags + "");
+    });
+});
+
+describe('Deleting new Community', () => {
+  test('API returns a successful response - code 200', async() => {
+          response = await community.deleteCommunity(newComTitle, token);
+          expect(response.status).toBe(200);
+  });
+});
---
+++ app/server/tests/api-setCommunityTags.test.js
@@ -0,0 +1,95 @@
+let { CommunityFunctions } = require('./functions/communityFunctions.js');
+let { AuthFunctions } = require('./functions/authFunctions.js');
+let community = new CommunityFunctions ();
+let user = new AuthFunctions();
+let token = '';
+
+const welComTitle = "Welcome";
+
+const newComTitle = "hello create";
+const newComDescript = "world";
+const newComRules = "1. rules";
+const newComTags = [{
+    "tag": "Informative",
+    "count": 1
+}];
+
+const updatedTags = "new";
+const updatedTags2 = "new new";
+
+
+describe('Logging in the test user', () => {
+  test('API returns a successful response - code 201', async () => {
+    let response = await user.login('test2', 'Tester123!');
+    token = response.body.token;
+    expect(response.status).toBe(201);
+  });
+});
+
+
+// Testing the get communities function after logging in
+describe('Get Communities Tags - After Login with welcome community', () => {
+    let response = '';
+  
+    beforeAll( async() => {
+      response = await community.getCommunityTags(welComTitle, token);
+    });
+  
+    test('API returns a successful response - code 200', () => {
+      expect(response.status).toBe(200);
+    });
+  
+    test('API returns the "Welcome" community tags', () => {
+      expect(" " + response.text+ " ").toContain("[]");
+    });
+});
+  
+describe('Creating new Community', () => {
+  test('API returns a successful response - code 201', async() => {
+    await community.deleteCommunity(newComTitle, token);
+    let response = await community.createCommunity(newComTitle, newComDescript, newComRules, newComTags, token);
+    expect(response.status).toBe(201);
+  });
+});
+
+// Testing the get communities function after logging in
+describe('Set Communities tags - After Login on new community', () => {
+    let response = '';
+
+    beforeAll( async() => {
+        response = await community.setCommunityTags(newComTitle, updatedTags, token);
+    });
+
+    test('API returns a successful response - code 204', () => {
+        expect(response.status).toBe(204);
+    });
+
+    test('API returns the "Welcome" community tags in its response body', () => {
+        expect(" " + response.text + " ").toContain(updatedTags);
+    });
+});
+
+describe('Get Community by Title - With Login, testing with new " " Community', () => {
+    let response = '';
+  
+    beforeAll(async() => {
+      response = await community.getCommunitybyTitle(newComTitle, token);
+    });
+  
+    test('API returns a successful response - code 200', () => {
+      expect(response.status).toBe(200);
+    });
+  
+    test('API returns description -  includes new community Title',() => {
+      expect('' + response.text + '').toContain(updatedTags);
+    });
+  });
+  
+
+
+describe('Deleting new Community', () => {
+  test('API returns a successful response - code 200', async() => {
+    let response = await community.deleteCommunity(newComTitle, token);
+    expect(response.status).toBe(200);
+  });
+});
