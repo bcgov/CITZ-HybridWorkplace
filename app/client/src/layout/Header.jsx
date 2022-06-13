255ba38f891107790ab5a843a4052839a0f24849
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2022-06-13 02:53:24 -0700

    [ref: hwp-349] Remove avatar and search bar
    - If the user is not logged in, then the user avatar and search bar are not visible

--- app/client/src/layout/Header.jsx
+++ app/client/src/layout/Header.jsx
@@ -31,77 +31,133 @@ import {
   InputBase,
   Avatar,
   Badge,
-  Stack,
   Icon,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Drawer,
 } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import PeopleIcon from "@mui/icons-material/People";
+import ForumIcon from "@mui/icons-material/Forum";
 import SearchIcon from "@mui/icons-material/Search";
+import LogoutIcon from "@mui/icons-material/Logout";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import "./header.css";
 import BCLogo from "./icons/BCLogo.svg";
 import SideMenu from "./SideMenu";
 import { connect } from "react-redux";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
-
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    backgroundColor: "#44b700",
-    color: "#44b700",
-    boxShadow: `0 0 0 1px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      content: '""',
-    },
-  },
-}));
-
-const menuId = "account-avatar";
 // props: {darkMode, setDarkMode}
 // <SideMenu darkMode={darkMode} setDarkMode={setDarkMode}/>
 const Header = (props) => {
+  const [menuOpen, setMenuOpen] = React.useState({ right: false });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setMenuOpen({ menuOpen, anchor: open });
+  };
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
+  }));
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      backgroundColor: "#44b700",
+      color: "#44b700",
+      boxShadow: `0 0 0 1px ${theme.palette.background.paper}`,
+      "&::after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "50%",
+        content: '""',
+      },
+    },
+  }));
+
+  const menuId = "account-avatar";
+
+  const menuItems = ["Home", "Posts", "Communities", "Profile", "Log Out"];
+  const menu = (anchor) => (
+    <Box
+      sx={{
+        display: { xs: "none", md: "flex" },
+      }}
+    >
+      <List>
+        {menuItems.map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index === 0 ? (
+                  <HomeIcon />
+                ) : index === 1 ? (
+                  <ForumIcon />
+                ) : index === 2 ? (
+                  <PeopleIcon />
+                ) : index === 3 ? (
+                  <PersonIcon />
+                ) : (
+                  <LogoutIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <Box
       sx={{
@@ -144,61 +200,67 @@ const Header = (props) => {
             The Neighbourhood
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-          <Box
-            sx={{
-              display: { xs: "none", md: "flex" },
-              mr: "2%",
-            }}
-          >
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <StyledBadge
-                overlap="circular"
-                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                variant="dot"
-              >
-                <Avatar
-                  alt="user account avatar"
-                  src="https://source.unsplash.com/random/150×150/?profile%20picture"
-                  sx={{
-                    boxShadow: `0 0 0 2px #FFF`,
-                  }}
+
+          {props.auth.accessToken !== "" && (
+            <>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
                 />
-              </StyledBadge>
-            </IconButton>
-          </Box>
-          <Box
-            sx={{
-              display: { xs: "none", md: "flex" },
-            }}
-          >
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{ mr: 2 }}
-            >
-              {props.auth.accessToken !== "" && (
+              </Search>
+              <Box
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  mr: "2%",
+                }}
+              >
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  color="inherit"
+                >
+                  <StyledBadge
+                    overlap="circular"
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    variant="dot"
+                  >
+                    <Avatar
+                      alt="user account avatar"
+                      src="https://source.unsplash.com/random/150×150/?profile%20picture"
+                      sx={{
+                        boxShadow: `0 0 0 2px #FFF`,
+                      }}
+                    />
+                  </StyledBadge>
+                </IconButton>
+              </Box>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer("right", false)}
+                onKeyDown={toggleDrawer("right", false)}
+                sx={{ mr: 2 }}
+              >
                 <MenuRoundedIcon fontSize="large" />
-              )}
-            </IconButton>
-          </Box>
+              </IconButton>
+              <Drawer
+                anchor="right"
+                open={menuOpen["right"]}
+                onClose={toggleDrawer("right", false)}
+              >
+                {menu("right")}
+              </Drawer>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
