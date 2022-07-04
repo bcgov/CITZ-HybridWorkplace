da0f7751864a529dec48ef16793ea997a1cbd411
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2022-07-04 12:21:40 -0700

    Matched randomizer regex to api regex.

--- app/server/versions/v1/tests/functions/randomizer.js
+++ app/server/versions/v1/tests/functions/randomizer.js
@@ -1,12 +1,9 @@
 const RandExp = require('randexp');
 
 // Authorization fields
-let oldPassword = new RandExp(/^(?=.*\d)(?=.*[\W]).{8,20}$/); // generate good random passwords
 let name = new RandExp(/^[A-Za-z]{8}$/); // generate random names, for IDIR field
-let oldEmail = new RandExp(/^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/); // generate valid emails
-
-let password = new RandExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,12}$/); // generate good random passwords
-let email = new RandExp(/^\w{5,10}([\.-]\w{1,10}){0,5}@\w{1,10}([\.-]?\w{1,10}){1,10}(\.\w{2,3}){1,3}$/); // generate valid emails
+let password = new RandExp(/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).{8}$/); // generate good random passwords
+let email = new RandExp(/^\w+([\\.-]\w+)*@\w+([\\.-]\w+)*(\.\w{2,3})+$/); // generate valid emails
 
 // Numbers
 let positive = new RandExp(/^\d*\.\d+$/);
