1744e5d34738224ebf9c6fcf89845f0e4ddd4e94
ZachBourque <zachbourque01@gmail.com>
2022-07-14 23:38:38 -0700

    Added comments to communityHelpers.js

--- app/client/src/helperFunctions/communityHelpers.js
+++ app/client/src/helperFunctions/communityHelpers.js
@@ -1,11 +1,30 @@
+/**
+ *
+ * @param {String} userId
+ * @param {Object} community
+ * @returns Boolean representing if the user is in the given community
+ */
 export const isUserInCommunity = (userId, community) => {
   return community.members.includes(userId);
 };
 
+/**
+ *
+ * @param {String} userId
+ * @param {Object} community
+ * @returns Boolean representing if the user is a moderator of the given community
+ */
 export const isUserModerator = (userId, community) => {
   return !!community.moderators.find((mod) => mod.userId === userId);
 };
 
+/**
+ *
+ * @param {String} userId
+ * @param {Object} community
+ * @returns Community Object with a userIsInCommunity boolean and a userIsModerator boolean
+ * @todo //TODO: Add more simplified fields in the community object to aid in use of data
+ */
 export const reshapeCommunityForFrontend = (userId, community) => {
   return {
     ...community,
@@ -14,6 +33,13 @@ export const reshapeCommunityForFrontend = (userId, community) => {
   };
 };
 
+/**
+ *
+ * @param {String} userId
+ * @param {Array} communities
+ * @returns Array of community objects with userIsInCommunity and userIsModerator boolean on each object
+ * @todo //TODO: Add more simplified fields in the community object to aid in use of data
+ */
 export const reshapeCommunitiesForFrontend = (userId, communities) => {
   return communities.map((comm) => ({
     ...comm,
