89aaeb074bd8e4521b02899a3c068ffb4e5e08f9
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2022-08-01 16:58:20 -0700

    Undoes the refactor on the Croutons component as it was unnecessary.

--- app/client/src/components/Croutons.jsx
+++ app/client/src/components/Croutons.jsx
@@ -9,28 +9,7 @@ const Croutons = (props) => {
 
   return (
     <Box role="presentation" onClick={handleClick} mb={3}>
-      {props.profile === true ? (
-        <Breadcrumbs>
-          <Link
-            variant="body2"
-            component="button"
-            underline="hover"
-            sx={{ display: "flex", alignItems: "center" }}
-            color="inherit"
-            onClick={() => navigate("/")}
-          >
-            <HomeRounded sx={{ mr: 0.5 }} fontSize="inherit" />
-            Home
-          </Link>
-          <Typography
-            sx={{ display: "flex", alignItems: "center" }}
-            color="text.primary"
-          >
-            <WebRounded sx={{ mr: 0.5 }} fontSize="inherit" />
-            {props.firstCrumb}
-          </Typography>
-        </Breadcrumbs>
-      ) : props.secondCrumb ? (
+      {props.secondCrumb ? (
         <Breadcrumbs>
           <Link
             variant="body2"
